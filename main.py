fc7a26a14164a6ce909f26c93b257ac86fe64296
Tom <57681416+tommckee96@users.noreply.github.com>
2022-03-09 21:54:14 +0000

    Added generate_frequency_count function to generate a frequency count of a string and write to a JSON file.
    Called generate_frequency_count in main with <main> tag containing the privacy policy as an argument.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import urllib.request
+
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -5,28 +7,63 @@ import json
 
 CONST_INDEX_DOMAIN = "cfcunderwriting.com"
 CONST_INDEX_URL = "https://www." + CONST_INDEX_DOMAIN
+CONST_EXTERNAL_RESOURCES_LIST_FILENAME = 'external_resources.json'
+CONST_WORD_FREQUENCY_COUNT_FILENAME = 'word_frequency_count.json'
 
 
 def main():
     index_page = requests.get(CONST_INDEX_URL)
     index_page_soup = BeautifulSoup(index_page.content, "html.parser")
 
-    write_external_resources_to_json_file(index_page_soup)
+    write_external_resources_to_json_file(index_page_soup, CONST_EXTERNAL_RESOURCES_LIST_FILENAME)
 
     privacy_policy_subdirectory = get_privacy_policy_subdirectory(index_page_soup)
 
     privacy_policy_url = CONST_INDEX_URL + privacy_policy_subdirectory
 
-    privacy_policy_page = requests.get(privacy_policy_url).text
-    privacy_policy_page_soup = BeautifulSoup(privacy_policy_page, "html.parser")
-    #print(privacy_policy_page_soup.get_text())
+    privacy_policy_page = requests.get(privacy_policy_url)
+    privacy_policy_page_soup = BeautifulSoup(privacy_policy_page.content, "html.parser")
 
-    data = privacy_policy_page_soup.findAll(text=True)
+    main_soup = privacy_policy_page_soup.find(id='main')
+
+    generate_frequency_count(main_soup.text, CONST_WORD_FREQUENCY_COUNT_FILENAME)
 
 
+def generate_frequency_count(text, json_filename):
+    """Writes a word frequency count to a JSON file
 
-def write_external_resources_to_json_file(soup):
-    """Function to write list of all external resources on page to JSON file"""
+    Parameters:
+        :text - string that the word frequency count will be generated from
+        :json_filename - string with name of JSON file that frequency count will be written to
+    """
+
+    # Remove all words containing a number or @ symbol such as postcodes and email addresses
+    text = ' '.join(s for s in text.split() if not any(c.isdigit() or c == '@' for c in s))
+
+    # Replace all non-alphabetic characters with a space
+    text = re.sub('[^a-zA-Z]+', ' ', text.lower())
+
+    word_list = text.split()
+
+    # Generate word and frequency pair dictionary
+    freq_count = {}
+    for word in word_list:
+        if word in freq_count:
+            freq_count[word] = freq_count[word] + 1
+        else:
+            freq_count[word] = 1
+
+    with open(json_filename, 'w', encoding='utf-8') as f:
+        json.dump(freq_count, f, indent=4)
+
+
+def write_external_resources_to_json_file(soup, json_filename):
+    """Function to write list of all external resources on page to JSON file
+
+    Parameters:
+    :soup - BeautifulSoup4 object representation of HTML page
+    :json_filename - string containing name of JSON file that will be written to
+    """
 
     # Construct list of dicts containing attributes of each element containing a link to an external resource
     ext_res_dict_list = []
@@ -38,12 +75,12 @@ def write_external_resources_to_json_file(soup):
         elif 'src' in tag.attrs:
             link = tag['src']
 
-        if link[:4] == 'http' and re.compile(CONST_INDEX_DOMAIN).search(link) is None:
+        resource_is_external = re.compile(CONST_INDEX_DOMAIN).search(link) is None
+        if link[:4] == 'http' and resource_is_external:
             ext_res_dict_list.append(tag.attrs)
 
-
-    # Write dict list to json file
-    with open('external_resources.json', 'w', encoding='utf-8') as f:
+    # Write dict list to json file (list of json)
+    with open(json_filename, 'w', encoding='utf-8') as f:
         json.dump(ext_res_dict_list, f, ensure_ascii=False, indent=4)
 
 
