cf6084d85b3b9d2a2dfacd6c9c772e85faa6a089
Tom <57681416+tommckee96@users.noreply.github.com>
2022-03-09 15:58:12 +0000

    Optimised write_external_resources_to_json_file so that it only loops through the BeautfulSoup tags once.
    Fixed issue in write_external_resources_to_json_file where non-external resources were being written to the json file

--- main.py
+++ main.py
@@ -3,32 +3,44 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-CONST_INDEX_URL = "https://www.cfcunderwriting.com"
+CONST_INDEX_DOMAIN = "cfcunderwriting.com"
+CONST_INDEX_URL = "https://www." + CONST_INDEX_DOMAIN
 
 
 def main():
     index_page = requests.get(CONST_INDEX_URL)
+    index_page_soup = BeautifulSoup(index_page.content, "html.parser")
 
-    soup = BeautifulSoup(index_page.content, "html.parser")
+    write_external_resources_to_json_file(index_page_soup)
 
-    write_external_resources_to_json_file(soup)
-
-    privacy_policy_subdirectory = get_privacy_policy_subdirectory(soup)
+    privacy_policy_subdirectory = get_privacy_policy_subdirectory(index_page_soup)
 
     privacy_policy_url = CONST_INDEX_URL + privacy_policy_subdirectory
 
-    print(privacy_policy_url)
+    privacy_policy_page = requests.get(privacy_policy_url).text
+    privacy_policy_page_soup = BeautifulSoup(privacy_policy_page, "html.parser")
+    #print(privacy_policy_page_soup.get_text())
+
+    data = privacy_policy_page_soup.findAll(text=True)
+
 
 
 def write_external_resources_to_json_file(soup):
-    """Function to write list all external resources on page to JSON file"""
+    """Function to write list of all external resources on page to JSON file"""
 
     # Construct list of dicts containing attributes of each element containing a link to an external resource
     ext_res_dict_list = []
-    for tag in soup.find_all(src=re.compile('^http')):
-        ext_res_dict_list.append(tag.attrs)
-    for tag in soup.find_all(href=re.compile('^http')):
-        ext_res_dict_list.append(tag.attrs)
+
+    for tag in soup.find_all():
+        link = ''
+        if 'href' in tag.attrs:
+            link = tag['href']
+        elif 'src' in tag.attrs:
+            link = tag['src']
+
+        if link[:4] == 'http' and re.compile(CONST_INDEX_DOMAIN).search(link) is None:
+            ext_res_dict_list.append(tag.attrs)
+
 
     # Write dict list to json file
     with open('external_resources.json', 'w', encoding='utf-8') as f:
